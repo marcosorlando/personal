d27570f333bacf064dbe21172922f6037de8361c
Amanda <amanda.cavalcanti06@gmail.com>
2023-03-22 20:11:39 -0300

    Adiciona Typed.js no titulo da Home

---
+++ _js/scripts.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.multiple-text', {
+    strings: [
+        'Desenvolvedor Web', 'Desenvolvedor PHP', 'Desenvolvedor Front-end'
+    ],
+    typedSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
