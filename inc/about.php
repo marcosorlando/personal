b14276f64860594c8ce103e001abfe3f81cca38c
marcosorlando <marcos@zen.ppg.br>
2023-03-27 20:36:38 -0300

    Update: About bg-color

--- inc/about.php
+++ inc/about.php
@@ -5,13 +5,14 @@
     </div>
 
     <div class="about-content">
-        <h2 class="heading"> Sobre <span>Mim</span></h2>
+        <h2 class="heading">Sobre <span>Mim</span></h2>
         <h3>Fronteded Developer</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolore illum ipsam, porro recusandae
             totam vero voluptatem? Accusantium dolorem
             ducimus eos inventore iste magnam odit rem repellendus soluta voluptatibus. At cupiditate deleniti eius
             explicabo ipsum nisi nobis quaerat rem sapiente
             sequi. Consectetur eaque laudantium veniam! Iure!</p>
+
         <a href="#" class="btn">Leia Mais</a>
     </div>
 </section>
